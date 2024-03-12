7f8534500bcd187e119195c4fde37ef312bc40ac
IvanNaum <nuk04@mail.ru>
2024-03-12 23:07:14 +0300

    Добавлены черновики для инпутов разных чатов

--- src/Components/Chat/Chat.js
+++ src/Components/Chat/Chat.js
@@ -2,4 +2,16 @@ import { BaseComponent } from '../BaseComponent.js';
 
 export default class Chat extends BaseComponent {
     templateName = 'Chat';
+
+    render() {
+        super.render();
+
+        this.getParent()
+            .querySelector('#input_message')
+            .addEventListener('input', this.getConfig().inputMessaegHandler);
+    }
+
+    setInputMessageValue(value) {
+        this.getParent().querySelector('#input_message').value = value;
+    }
 }
