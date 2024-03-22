18dfadbea2631bf08208b856ff183891270ad80f
IvanNaum <nuk04@mail.ru>
2024-03-22 23:43:39 +0300

    Добавлена кнопка 'назад' в профиле

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,9 +1,21 @@
 import { BaseComponent } from '../BaseComponent.js';
-
+import { goToPage } from '../../utils/goToPage.js';
+import ChatPage from '../../Pages/ChatPage.js';
 /**
  * Рендерит форму для логина или регистрации
  * @class Класс компонента формы
  */
 export default class Profile extends BaseComponent {
     templateName = 'Profile';
+
+    render() {
+        super.render();
+
+        this.getParent()
+            .querySelector('#backButton')
+            .addEventListener('click', () => {
+                // TODO: Возврат назад
+                goToPage(ChatPage);
+            });
+    }
 }
