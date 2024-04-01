62d868d13f01c7f565014bd328a9d611efc4bd30
IvanNaum <nuk04@mail.ru>
2024-04-02 01:08:27 +0300

    Добавлена страница добавления групп

--- src/Components/ChatList/ChatList.js
+++ src/Components/ChatList/ChatList.js
@@ -3,6 +3,7 @@ import ChatListItem from '../ChatListItem/ChatListItem.js';
 import { goToPage } from '../../utils/goToPage.js';
 import ProfilePage from '../../Pages/ProfilePage.js';
 import ContactsPage from '../../Pages/ContactsPage.js';
+import CreateGroupPage from '../../Pages/CreateGroupPage.js';
 
 /**
  * Рендерит компоненты боковой панели: заголовок, поиск, список чатов, пользователь и выйти
@@ -26,6 +27,11 @@ export default class ChatList extends BaseComponent {
             .querySelector('#contacts_btn')
             .addEventListener('click', () => {
                 goToPage(ContactsPage);
+            });
+        this.getParent()
+            .querySelector('#create_group_btn')
+            .addEventListener('click', () => {
+                goToPage(CreateGroupPage);
             });
     }
 
