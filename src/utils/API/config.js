8d3723c3d1d88dc814d157985ce0afb86984be93
IvanNaum <nuk04@mail.ru>
2024-04-27 18:25:11 +0300

    fix url

--- src/utils/API/config.js
+++ src/utils/API/config.js
@@ -4,5 +4,5 @@ export let CSATbaseUrl = 'localhost:8081';
 
 if (protocol === 'https') {
     baseUrl = 'chatme.site/api/v1';
-    CSATbaseUrl = 'chatme.site/api/v1';
+    CSATbaseUrl = 'chatme.site/api/v2';
 }
