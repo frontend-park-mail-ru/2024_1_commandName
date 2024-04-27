c2b3a9a186d439b0fad7a4872f79ce72f2ea0c98
IvanNaum <nuk04@mail.ru>
2024-04-27 15:09:33 +0300

    Фикс стилей звезд

--- CSAT/src/Components/Stars/Stars.js
+++ CSAT/src/Components/Stars/Stars.js
@@ -5,4 +5,29 @@ import { BaseComponent } from '../BaseComponent.js';
  */
 export default class Stars extends BaseComponent {
     templateName = 'Stars';
+
+    render() {
+        super.render();
+
+        const stars = document.querySelectorAll('.csat__star');
+
+        stars.forEach((star, index) => {
+            star.addEventListener('click', () => {
+                // Устанавливаем рейтинг
+                const rating = index + 1;
+
+                // Обновляем внешний вид звезд
+                for (let i = 0; i < stars.length; i++) {
+                    if (i < rating) {
+                        stars[i].innerHTML = '★';
+                    } else {
+                        stars[i].innerHTML = '☆';
+                    }
+                }
+
+                // Сохраняем рейтинг в хранилище (например, в localStorage)
+                localStorage.setItem('csatRating', rating);
+            });
+        });
+    }
 }
