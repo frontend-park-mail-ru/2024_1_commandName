d8b14a12f70c79f61944f8ee451243770ddc1f22
IvanNaum <nuk04@mail.ru>
2024-04-27 10:59:57 +0300

    Добавлена базовая структура csat

---
+++ CSAT/src/Components/Quetions/Quetions.js
@@ -0,0 +1,8 @@
+import { BaseComponent } from '../BaseComponent.js';
+/**
+ * Рендерит TODO
+ * @class Класс TODO
+ */
+export default class Quetionts extends BaseComponent {
+    templateName = 'Quetionts';
+}
