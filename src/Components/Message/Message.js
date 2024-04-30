60071100761a7cc7c5ebca8d8d0381e11698460d
alex <volohovaleksandr1974@gmail.com>
2024-04-30 22:53:07 +0300

    удаление сообщений

--- src/Components/Message/Message.js
+++ src/Components/Message/Message.js
@@ -1,12 +1,36 @@
 import { BaseComponent } from '../BaseComponent.js';
 
-/**
- * Рендерит сообщение
- * @class Класс компонента сообщения
- */
 export default class Message extends BaseComponent {
     templateName = 'Message';
+    #messageElement;
+    #ContextMenu;
+
     render() {
         super.render();
+
+        this.#messageElement = this.getParent().querySelector(
+            `#message_id_${this.getConfig().message_id}`,
+        );
+
+        if (!this.#messageElement.classList.contains('my_message')) {
+            return;
+        }
+
+        this.#ContextMenu = this.#messageElement.querySelector(`#ContextMenu`);
+
+        this.#messageElement.addEventListener('contextmenu', (event) => {
+            event.preventDefault();
+            this.#ContextMenu.style.display = 'block';
+        });
+        this.#ContextMenu
+            .querySelector('.delete-button')
+            .addEventListener('click', () => {
+                this.#messageElement.style.display = 'none';
+                // TODO: отправка запроса на удаление
+            });
+
+        document.addEventListener('click', () => {
+            this.#ContextMenu.style.display = 'none';
+        });
     }
 }
