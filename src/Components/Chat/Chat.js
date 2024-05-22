bc80edc0b3f0f0d757732a517966edc3a3e62a9d
alex <volohovaleksandr1974@gmail.com>
2024-05-22 12:02:40 +0300

    minor fix

--- src/Components/Chat/Chat.js
+++ src/Components/Chat/Chat.js
@@ -6,11 +6,6 @@ export default class Chat extends BaseComponent {
     #searchMessages;
 
     render() {
-        // this.ws_sendMesasge = new WebSocketManager(
-        //     'sendMessage',
-        //     this.getConfig().getMessage,
-        // );
-
         this.getConfig().header = 'Выберите чат';
         if (this.getConfig().type === '/channel') {
             this.getConfig().header = 'Выберите канал';
@@ -28,30 +23,5 @@ export default class Chat extends BaseComponent {
             getSearch: this.getConfig().getSearchMessages,
         });
         this.#searchMessages.render();
-
-        // this.getParent()
-        //     .querySelector('#input_message')
-        //     .addEventListener('input', this.getConfig().inputMessage);
-        //
-        // this.getParent()
-        //     .querySelector('#input_message')
-        //     .addEventListener('keydown', (event) => {
-        //         if (event.keyCode === 13) {
-        //             event.preventDefault();
-        //             this.getConfig().sendMessage();
-        //         }
-        //     });
-        //
-        // this.getParent()
-        //     .querySelector('.input_send')
-        //     .addEventListener('click', this.getConfig().sendMessage);
-    }
-
-    // getMessageSocket() {
-    //     return this.ws_sendMesasge;
-    // }
-
-    setInputMessageValue(value) {
-        this.getParent().querySelector('#input_message').value = value;
     }
 }
