89fac8ce56e147394817209c5603f41277eb8662
alex <volohovaleksandr1974@gmail.com>
2024-05-30 15:16:08 +0300

    fix sw

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -30,18 +30,4 @@ self.addEventListener('fetch', (event) => {
     );
 });
 
-self.addEventListener('activate', (event) => {
-    const cacheWhitelist = [CACHE_NAME];
-
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cacheName) => {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName);
-                    }
-                }),
-            );
-        }),
-    );
-});
+self.addEventListener('activate', () => {});
