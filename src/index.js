6aaeae8ac737396bd61700787a4214809b93e89d
alex <volohovaleksandr1974@gmail.com>
2024-05-30 23:52:19 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -18,15 +18,17 @@ function registerServiceWorker() {
         navigator.serviceWorker
             .getRegistrations()
             .then(function (registrations) {
-                if (registrations.length === 0) {
+                console.log(registrations);
+                if (registrations.length === 0 || registrations.length === 1) {
                     return navigator.serviceWorker.register(
                         new URL('./serviceWorker.js', import.meta.url),
                     );
                 } else {
-                    const serviceWorker = registrations[0];
-                    if (!serviceWorker.active) {
-                        return serviceWorker.activate();
-                    }
+                    registrations.forEach((sw) => {
+                        if (!sw.active) {
+                            return sw.activate();
+                        }
+                    });
                 }
             })
             .catch(function (err) {
